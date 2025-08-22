e101d8ddc3d2f5e003827c7525589dfa81a3efd5
Hakimrabet <a.hakim@mydigipay.com>
2025-08-22 23:34:18 +0330

    cleaning

--- src/test/java/com/bill/user/api/rest/dong/ExpenseControllerIT.java
+++ src/test/java/com/bill/user/api/rest/dong/ExpenseControllerIT.java
@@ -2,7 +2,6 @@ package com.bill.user.api.rest.dong;
 
 import com.bill.user.AbstractContainerBaseTest;
 import com.bill.user.api.rest.dong.model.request.AddExpenseRequest;
-import com.bill.user.api.rest.dong.model.request.EditExpenseRequest;
 import com.bill.user.api.rest.dong.model.request.SplitDto;
 import com.bill.user.api.rest.dong.model.response.*;
 import com.bill.user.common.GeneralResponse;
@@ -40,6 +39,7 @@ class ExpenseControllerIT extends AbstractContainerBaseTest {
 
     @BeforeEach
     void setUp() {
+        groupDao.deleteAll();
         expenseDao.deleteAll();
     }
 
@@ -89,65 +89,6 @@ class ExpenseControllerIT extends AbstractContainerBaseTest {
         assertThat(response.getBody().getExpense().getGroupId()).isEqualTo("group-id");
         assertThat(response.getBody().getExpense().getAmount()).isEqualTo(300000L);
         assertThat(response.getBody().getExpense().getDescription()).isEqualTo("joje");
-        assertThat(response.getBody().getExpense().getType()).isEqualTo(Type.SHOP);
-        assertThat(response.getBody().getExpense().getSplits()).hasSize(3);
-
-        assertThat(response.getBody().getExpense().getSplits().get(0).getCreditAmount()).isEqualTo(200000L);
-        assertThat(response.getBody().getExpense().getSplits().get(0).getDebtAmount()).isEqualTo(0L);
-
-        assertThat(response.getBody().getExpense().getSplits().get(1).getCreditAmount()).isEqualTo(0L);
-        assertThat(response.getBody().getExpense().getSplits().get(1).getDebtAmount()).isEqualTo(100000L);
-
-        assertThat(response.getBody().getExpense().getSplits().get(2).getCreditAmount()).isEqualTo(0L);
-        assertThat(response.getBody().getExpense().getSplits().get(2).getDebtAmount()).isEqualTo(100000L);
-    }
-
-    @Test
-    void editExpense() {
-        createGroupWithExpense();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("X-User-Id", "1");
-
-        EditExpenseRequest request = new EditExpenseRequest();
-        request.setAmount(300_000L);
-        request.setDescription("morgh");
-        request.setGroupId("group-id");
-        request.setType(Type.SHOP);
-
-        SplitDto dto1 = new SplitDto();
-        dto1.setUserId(1L);
-        dto1.setExpenseId("expense-id");
-        dto1.setCreditAmount(200_000L);
-        dto1.setDebtAmount(0L);
-
-        SplitDto dto2 = new SplitDto();
-        dto2.setUserId(2L);
-        dto2.setExpenseId("expense-id");
-        dto2.setCreditAmount(0L);
-        dto2.setDebtAmount(100_000L);
-
-        SplitDto dto3 = new SplitDto();
-        dto3.setUserId(1L);
-        dto3.setExpenseId("expense-id");
-        dto3.setCreditAmount(0L);
-        dto3.setDebtAmount(100_000L);
-
-        request.setSplits(List.of(dto1, dto2, dto3));
-
-        HttpEntity<Object> entity = new HttpEntity<>(request, headers);
-
-        ResponseEntity<ExpenseResponse> response = restTemplate.exchange("/split/expense/expense-id",
-                HttpMethod.PUT, entity, ExpenseResponse.class);
-
-        assertThat(response).isNotNull();
-        assertThat(response.getBody().getResult().getTitle()).isEqualTo(ResultStatus.SUCCESS);
-        assertThat(response.getBody().getExpense()).isNotNull();
-
-        assertThat(response.getBody().getExpense().getExpenseId()).isNotBlank();
-        assertThat(response.getBody().getExpense().getGroupId()).isEqualTo("group-id");
-        assertThat(response.getBody().getExpense().getAmount()).isEqualTo(300000L);
-        assertThat(response.getBody().getExpense().getDescription()).isEqualTo("morgh");
         assertThat(response.getBody().getExpense().getType()).isEqualTo(Type.SHOP);
         assertThat(response.getBody().getExpense().getSplits()).hasSize(3);
 
